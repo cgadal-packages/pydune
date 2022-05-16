a4f38e0e555b44a72b4c23e594c16212d9078538
Cyril Gadal <cyril.gadal@gmail.com>
2022-05-16 10:36:24 +0200

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup, find_packages
 
-setup(name="PyDune", packages=find_packages())
+setup(name="PyDune",
+      packages=find_packages(),
+      python_requires='>=3',
+      install_requires=[
+        "numpy", "matplotlib", "cdsapi", "scipy", "datetime",
+        "windrose", "xhistogram", "itertools", "decimal"],
+      url='https://cgadal.github.io/PyDune/',
+      author='Cyril Gadal',
+      license='Apache-2.0',
+      version='0.1'
+      )

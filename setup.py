f759a24e1d4f8dfe483c0afee30df3cd1ced5959
Cyril Gadal <cyril.gadal@gmail.com>
2022-05-16 10:50:45 +0200

    adding install instructions to Readme

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ setup(name="PyDune",
       python_requires='>=3',
       install_requires=[
         "numpy", "matplotlib", "cdsapi", "scipy", "datetime",
-        "windrose", "xhistogram", "wheel"],
+        "windrose", "xhistogram"],
       url='https://cgadal.github.io/PyDune/',
       author='Cyril Gadal',
       license='Apache-2.0',
-      version='0.1'
+      version='0.1',
+      zip_safe=False,
       )

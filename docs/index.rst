99f6945d0cf2e611e63ba8f42a662b549850f0c4
Cyril Gadal <cyril.gadal@gmail.com>
2023-08-07 15:07:41 +0200

    Fixing documentation building

---
+++ docs/index.rst
@@ -0,0 +1,25 @@
+.. PyDune documentation master file, created by
+   sphinx-quickstart on Fri May 14 16:27:32 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to pydune's documentation!
+========================================
+
+.. toctree::
+    :maxdepth: 1
+    :caption: Contents:
+
+    Home page <self>
+    Examples and Tutorials <_examples/index.rst>
+    API reference <_autosummary/pydune>
+
+
+.. Indices and tables
+.. ==================
+..
+.. * :ref:`genindex`
+.. * :ref:`modindex`
+
+
+.. automodule:: pydune.physics
--- pydune/physics/turbulent_flow/flow_solver.py
+++ pydune/physics/turbulent_flow/flow_solver.py
@@ -2,10 +2,10 @@ r"""
 In this module, the function :func:`solve_turbulent_flow <PyDune.physics.turbulent_flow.flow_solver.solve_turbulent_flow>`
 solve  the turbulent flow on a sinusoidal bottom in different configurations:
 
-    * '1D_unbounded': 1D, unbounded turbulent flow (in practice capped by a rigid lid, that should be put far from the bed) [1, 2]
-    * '1D_freesurface': 1D, turbulent flow capped by a free surface (typically river configuration) [1, 2]
-    * '1D_freeatmosphere': 1D, turbulent flow capped by a stratified flow (typically atmopshere configuration) [3]
-    * '2D_unbounded': 2D, unbounded turbulent flow (in practice capped by a rigid lid, that should be put far from the bed) [1, 2, 3]
+- '1D_unbounded': 1D, unbounded turbulent flow (in practice capped by a rigid lid, that should be put far from the bed) [1, 2]
+- '1D_freesurface': 1D, turbulent flow capped by a free surface (typically river configuration) [1, 2]
+- '1D_freeatmosphere': 1D, turbulent flow capped by a stratified flow (typically atmopshere configuration) [3]
+- '2D_unbounded': 2D, unbounded turbulent flow (in practice capped by a rigid lid, that should be put far from the bed) [1, 2, 3]
 
 For details on the flow theoretical modelling, please refer to the references below.
 
@@ -13,11 +13,13 @@ For details on the flow theoretical modelling, please refer to the references be
 References
 ----------
 
-.. line-block::
-    [1] Fourrière, A. (2009). Morphodynamique des rivières: Sélection de la largeur, rides et dunes (Doctoral dissertation, Université Paris-Diderot-Paris VII).
-    [2] Fourriere, A., Claudin, P., & Andreotti, B. (2010). Bedforms in a turbulent stream: formation of ripples by primary linear instability and of dunes by nonlinear pattern coarsening. Journal of Fluid Mechanics, 649, 287-328.
-    [3] Andreotti, B., Fourriere, A., Ould-Kaddour, F., Murray, B., & Claudin, P. (2009). Giant aeolian dune size determined by the average depth of the atmospheric boundary layer. Nature, 457(7233), 1120-1123.
-    [4] Andreotti, B., Claudin, P., Devauchelle, O., Durán, O., & Fourrière, A. (2012). Bedforms in a turbulent stream: ripples, chevrons and antidunes. Journal of Fluid Mechanics, 690, 94-128.
+[1] Fourrière, A. (2009). Morphodynamique des rivières: Sélection de la largeur, rides et dunes (Doctoral dissertation, Université Paris-Diderot-Paris VII).
+
+[2] Fourriere, A., Claudin, P., & Andreotti, B. (2010). Bedforms in a turbulent stream: formation of ripples by primary linear instability and of dunes by nonlinear pattern coarsening. Journal of Fluid Mechanics, 649, 287-328.
+
+[3] Andreotti, B., Fourriere, A., Ould-Kaddour, F., Murray, B., & Claudin, P. (2009). Giant aeolian dune size determined by the average depth of the atmospheric boundary layer. Nature, 457(7233), 1120-1123.
+
+[4] Andreotti, B., Claudin, P., Devauchelle, O., Durán, O., & Fourrière, A. (2012). Bedforms in a turbulent stream: ripples, chevrons and antidunes. Journal of Fluid Mechanics, 690, 94-128.
 
 """
 
@@ -40,23 +42,23 @@ def solve_turbulent_flow(model, parameters, Kappa=0.4, max_z=None,
     Parameters
     ----------
     model : str
-        Chosen configuration. It can be '1D_unbounded', '1D_freesurface', '1D_freeatmosphere' or '2D_unbounded'.
+        Chosen configuration. It can be ``'1D_unbounded'``, ``'1D_freesurface'``, ``'1D_freeatmosphere'`` or ``'2D_unbounded'``.
     parameters : dict
-        Dictionnary containing the physical parameters necessary for solving each
-        models. For each model, the list of parameters the dictionnary must contain is:
-            * 1D_unbounded: 'eta_0', 'eta_H'
-            * 1D_freesurface: 'eta_0', 'eta_H', 'Fr'
-            * 1D_freeatmosphere: 'eta_0', 'eta_H', 'eta_B', 'Fr'
-            * 2D_unbounded: 'eta_0', 'eta_H', 'Fr', 'alpha'
+        Dictionnary containing the physical parameters necessary for solving each models. For each model, the list of parameters the dictionnary must contain is:
+
+        - 1D_unbounded: ``eta_0``, ``eta_H``
+        - 1D_freesurface: ``eta_0``, ``eta_H``, ``Fr``
+        - 1D_freeatmosphere: ``eta_0``, ``eta_H``, ``eta_B``, ``Fr``
+        - 2D_unbounded: ``eta_0``, ``eta_H``, ``Fr``, ``alpha``
     Kappa : float, optional
         Von Karmàn constant (the default is 0.4).
     max_z : float, optional
         Maximum vertical position where the system is solved, and also where the boundary conditons are applied.
         Usually set to something slightly smaller than `eta_H` to avoid the very slow resolution close to the top of the boundary layer.
-        Usefull when investigating the solution close to the bottom (the default is eta_H).
+        Usefull when investigating the solution close to the bottom (the default is ``eta_H``).
     method: str, optional
         Numerical method used to solve the equations. It is passed to the solver
-        :func:`solve_ivp <scipy.integrate.solve_ivp>` (default is 'DOP853', which corresponds to an Explicit Runge-Kutta method of order 8 with an adaptative time-step).
+        :func:`solve_ivp <scipy.integrate.solve_ivp>` (default is ``DOP853``, which corresponds to an Explicit Runge-Kutta method of order 8 with an adaptative time-step).
     atol,rtol : float, optional
         Absolute tolerance. The solver keeps the local error estimate smaller
         than :math:`atol + rtol*abs(y)`, where :math:`y` is the solution at a given time step.
@@ -72,11 +74,11 @@ def solve_turbulent_flow(model, parameters, Kappa=0.4, max_z=None,
         :math:`\eta = k z` (float, array_like). It is built from seleveral
         :class:`ODE_solution <scipy.integrate.OdeSolution>`. See corresponding
         documentation for more information about the interpolation algorithm. If
-        `model` is '1D_freeatmosphere', return also a function calculating the
-        streamfunction if the 1D_freeatmosphere (above eta_H). Its arguments are, in this order:
-            * vertical coordinate :math:`\eta = k z`, numpy array
-            * the horizontal coordinate :math:`\eta = k x`, numpy array,
-            * aspect ratio the bottom perturbation :math:`\eta = k \xi` (float)
+        ``model = '1D_freeatmosphere'``, return also a function calculating the
+        streamfunction above eta_H. Its arguments are, in this order:
+        - vertical coordinate :math:`\eta = k z`, numpy array
+        - the horizontal coordinate :math:`\eta = k x`, numpy array,
+        -   aspect ratio the bottom perturbation :math:`\eta = k \xi` (float)
 
 
     References
